b298e99766a653cc39afe32f9a15ef43b583972d
maryamchegini87 <m.chegini34@gmail.com>
2025-07-27 22:28:43 +0330

    develop accordion component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,83 @@
 import { useState } from "react";
 import "./App.css";
 import Accordion from "./components/Accordion/Accordion";
-
+const questions = [
+  { id: 1, question: "What is your name?", type: "text" },
+  {
+    id: 2,
+    question: "What is your gender?",
+    type: "radio",
+    options: [
+      { key: "FEMALE", text: "Female" },
+      { key: "MALE", text: "Male" },
+      { key: "OTHER", text: "Other" },
+    ],
+  },
+  { id: 3, question: "What is your the date of your birth?", type: "date" },
+  {
+    id: 4,
+    question: "What insurances do you have?",
+    type: "checkbox",
+    options: [
+      { key: "HEALTH", text: "Health" },
+      { key: "LIABILITY", text: "Liability" },
+      { key: "LEGAL", text: "Legal" },
+      { key: "CAR", text: "Car" },
+    ],
+  },
+  {
+    id: 5,
+    question: "What is your employment status?",
+    type: "select",
+    options: [
+      { key: "EMPLOYEE", text: "Employee" },
+      { key: "BUSINESS_OWNER", text: "Business Owner" },
+      { key: "HOUSE_SPOUSE", text: "Housewife / Househusband" },
+      { key: "RETIREE", text: "Retiree" },
+      { key: "STUDENT", text: "Student" },
+      { key: "SELF_EMPLOYED", text: "Self-Employed" },
+      { key: "UNEMPLOYED", text: "Unemployed" },
+    ],
+  },
+  { id: 6, question: "What is your phone number?", type: "text" },
+];
 function App() {
-  const [showContent, setShowContent] = useState(false);
+  const [openId, setOpenId] = useState(null);
+  const [userAnswers,setUserAnswers]=useState({})
+
+  function handleToggle(id) {
+    setOpenId((prevId) => (prevId === id ? null : id));
+  }
+
+  function handeleAnswerChange(questionId,answer){
+    setUserAnswers((prev)=>({
+      ...prev,
+      [questionId]:answer
+    }))
+  }
+
+  function handeleSubmit(e){
+    e.preventDefault();
+    console.log(userAnswers)
+  }
+
+
   return (
     <>
-      <form>
-        <Accordion showContent={showContent} setShowContent={setShowContent} />
+      <form onClick={handeleSubmit}>
+        {questions.map((question) => (
+          <div key={question.id}>
+            <hr />
+            <Accordion
+              showContent={openId===question.id}
+              question={question}
+              onToggle={()=>handleToggle(question.id)}
+              value={userAnswers[question.id]}
+              onChange={(answer)=>handeleAnswerChange(question.id,answer)}
+            />
+          </div>
+        ))}
+        <hr />
       </form>
     </>
   );
--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,17 +1,98 @@
 import styles from "./Accordion.module.css";
-export default function Accordion({ showContent, setShowContent }) {
+export default function Accordion({
+  question,
+  showContent,
+  onToggle,
+  value,
+  onChange,
+}) {
+  const inputRenderers = {
+    text: () => (
+      <input
+        type="text"
+        name={question.id}
+        value={value || ""}
+        onChange={(e) => onChange(e.target.value)}
+      />
+    ),
+    date: () => (
+      <input
+        type="date"
+        name={question.id}
+        value={value || ""}
+        onChange={(e) => onChange(e.target.value)}
+      />
+    ),
+    radio: () =>
+      question.options?.map((option) => (
+        <label key={option.key}>
+          <input
+            type="radio"
+            name={question.id}
+            value={option.key}
+            checked={value === option.key}
+            onChange={(e) => onChange(e.target.value)}
+          />
+
+          {option.text}
+        </label>
+      )),
+    checkbox: () =>
+      question.options?.map((option) => (
+        <label key={option.key}>
+          <input
+            type="checkbox"
+            name={`${question.id}-${option.key}`}
+            value={option.key}
+            checked={value?.includes(option.key)||false}
+            onChange={(e) => {
+              const isChecked = e.target.checked;
+              const newValue = isChecked
+              ?[...(value||[]),option.key]
+              :(value||[]).filter((v)=>v!== option.key)
+              onChange(newValue)
+            }}
+          />
+          {option.text}
+        </label>
+      )),
+    select: () => (
+      <select name={question.id} value={value||""}
+      onChange={(e)=>onChange(e.target.value)}>
+        <option value="">Select...</option>
+        {question.options?.map((option) => (
+          <option key={option.key} value={option.key}>
+            {option.text}
+          </option>
+        ))}
+      </select>
+    ),
+  };
+
+  const renderInputBytype = () =>
+    inputRenderers[question.type]?.() || <p>Unsupported input type!</p>;
   return (
     <>
-      <hr />
       <div className={styles.contentainer}>
-        <div onClick={() => setShowContent(!showContent)}>title</div>
-        {showContent && (
-          <div className={styles.content}>
-            <p>content</p>
+        <div className={styles.wrapper}>
+          <div
+            onClick={onToggle}
+            className={showContent ? styles.titleContent : styles.title}
+          >
+            {question.question}
           </div>
+          {!showContent && (
+            <div className={`${styles.circle} ${styles.blueCircle}`} />
+          )}
+        </div>
+        {showContent && (
+          <div className={styles.content}>{renderInputBytype()}</div>
         )}
+        <div>
+          <button className={styles.mainBtn}>Submit</button>
+          <button className={styles.cancleBtn}>Cancel</button>
+        </div>
       </div>
-        <hr />
     </>
   );
 }
